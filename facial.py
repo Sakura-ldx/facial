7853728013a6dd29464f8f2d1f4375843864ddc5
Sakura-ldx <68360726+Sakura-ldx@users.noreply.github.com>
2022-02-07 22:49:54 +0800

    Update facial.py

--- facial.py
+++ facial.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -34,7 +35,7 @@ class FacialPredictor(object):
     def __call__(self, imgs):
         img_num = len(imgs)
         batch_num = self.batch_num
-        pre_vals_list, pre_ids_list = [], []
+        # pre_vals_list, pre_ids_list = [], []
         for begin_img_no in range(0, img_num, batch_num):
             end_img_no = min(img_num, begin_img_no + batch_num)
             imgs_tf = [
@@ -49,57 +50,6 @@ class FacialPredictor(object):
             # else:
             #     outputs = outputs.unfold(0, 2, 2).mean(dim=2)
 
-            outputs = outputs > 0
-            pre_vals_list.append(outputs)
-        return torch.cat(pre_vals_list)
-
-
-class WeaponRecognizer(object):
-    def __init__(self, args):
-        self.model = (
-            create_model(
-                args.wp_model,
-                num_classes=args.wp_num_classes,
-                in_chans=args.in_chans,
-                pretrained=args.pretrained,
-                checkpoint_path=args.checkpoint_path,
-            )
-            .cuda()
-            .eval()
-        )
-        self.wp_rec_batch_num = args.wp_rec_batch_num
-
-    def transform_image(self, image):
-        my_transforms = transforms.Compose(
-            [
-                transforms.Resize(384),
-                transforms.CenterCrop(384),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ]
-        )
-        return my_transforms(image).unsqueeze(0)
-
-    def __call__(self, imgs):
-        img_num = len(imgs)
-        batch_num = self.wp_rec_batch_num
-        pre_vals_list, pre_ids_list = [], []
-        for begin_img_no in range(0, img_num, batch_num):
-            end_img_no = min(img_num, begin_img_no + batch_num)
-            imgs_tf = [
-                self.transform_image(imgs[ino])
-                for ino in range(begin_img_no, end_img_no)
-            ]
-            tensor = torch.cat(imgs_tf).cuda()
-            with torch.no_grad():
-                outputs = self.model.forward(tensor)
-            # if len(imgs_tf) == 1:
-            #     outputs = outputs.unfold(0, 1, 2).mean(dim=1)
-            # else:
-            #     outputs = outputs.unfold(0, 2, 2).mean(dim=2)
-
-            outputs = torch.nn.functional.softmax(outputs)
-            pre_vals, pre_ids = outputs.max(1)
-            pre_vals_list.append(pre_vals)
-            pre_ids_list.append(pre_ids)
-        return torch.cat(pre_vals_list), torch.cat(pre_ids_list)
+            outputs = (outputs > 0).cpu().numpy()
+            outputs = pd.DataFrame(outputs, columns=['glasses', 'gender', 'smile', 'age'])
+        return outputs
